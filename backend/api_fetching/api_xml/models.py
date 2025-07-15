3306e77ce0818a272bf1becd1652a3df49f2e509
ThasleemPeer D <thasleempeer361@gmail.com>
2025-07-15 11:47:30 +0530

    Create MYSQL database schema (ORM)

--- backend/api_fetching/api_xml/models.py
+++ backend/api_fetching/api_xml/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class xml_file(models.Model):
+    cpe_title=models.CharField(max_length=2000)
+    cpe_22_uri=models.TextField()
+    cpe_23_uri=models.TextField()
+    reference_links=models.TextField()
+    cpe_22_deprecation_date=models.DateField()
+    cpe_23_deprecation_date=models.DateField()
