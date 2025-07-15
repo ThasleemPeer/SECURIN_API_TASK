1125411c1b192843fda1b289b5ba5f884f29f9bf
ThasleemPeer D <thasleempeer361@gmail.com>
2025-07-15 11:35:41 +0530

    Database Connection Successful

--- backend/api_fetching/api_fetching/settings.py
+++ backend/api_fetching/api_fetching/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_xml',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,16 @@ WSGI_APPLICATION = 'api_fetching.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SECURIN_API',
+        'USER': 'root',
+        'PASSWORD': 'Peer@361',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
